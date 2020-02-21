0d39035aef24a240221c58d5111b6cd77c65b0b0
mbs9b7 <mbsobelman@gmail.com>
2020-02-21 13:23:22 -0600

    git add is important

---
+++ src/main/kotlin/com/mbsobelman/kotlinSpringPlayground/KotlinSpringPlaygroundApplication.kt
@@ -0,0 +1,11 @@
+package com.mbsobelman.kotlinSpringPlayground
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringPlaygroundApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinSpringPlaygroundApplication>(*args)
+}
